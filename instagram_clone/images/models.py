b0c43fee7e5cdd9b77d5442c2d03a609a3db4f7c
Tackhee Han <taki1984@naver.com>
2018-10-27 22:48:01 +0900

    #6 Create Image, Comment and Like models

--- instagram_clone/images/models.py
+++ instagram_clone/images/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from instagram_clone.users import models as user_models
 
-# Create your models here.
+
+class TimeStampModel(models.Model):
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Image(TimeStampModel):
+
+    ''' Image Model '''
+
+    file = models.ImageField()
+    location = models.CharField(max_length=140)
+    caption = models.TextField()
+    creator = models.ForeignKey(user_models.User, on_delete=models.PROTECT, null=True)
+
+
+class Comment(TimeStampModel):
+
+    ''' Comment Model '''
+
+    message = models.TextField()
+    creator = models.ForeignKey(user_models.User, on_delete=models.PROTECT, null=True)
+    image = models.ForeignKey(Image, on_delete=models.PROTECT, null=True)
+
+
+class Like(TimeStampModel):
+
+    ''' Like Model '''
+
+    creator = models.ForeignKey(user_models.User, on_delete=models.PROTECT, null=True)
+    image = models.ForeignKey(Image, on_delete=models.PROTECT, null=True)
