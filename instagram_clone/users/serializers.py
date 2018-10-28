c5a7d111db1890ca870266413f9a85c18a2377bf
Tackhee Han <taki1984@naver.com>
2018-10-28 23:54:04 +0900

    # get profile information.

--- instagram_clone/users/serializers.py
+++ instagram_clone/users/serializers.py
@@ -1,5 +1,24 @@
 from rest_framework import serializers
 from . import models
+from instagram_clone.images import serializers as image_serializers
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+
+    images = image_serializers.UserProfileImageSerializer(many=True)
+
+    class Meta:
+        model = models.User
+        fields = (
+            'username',
+            'name',
+            'bio',
+            'website',
+            'post_count',
+            'followers_count',
+            'following_count',
+            'images'
+        )
 
 
 class UserSerializer(serializers.ModelSerializer):
