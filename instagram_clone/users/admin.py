26f4c69ca9916a61db5653bc63e1d6307ed5782a
Tackhee Han <taki1984@naver.com>
2018-10-28 15:20:44 +0900

    # add followers, following field in admin for users model

--- instagram_clone/users/admin.py
+++ instagram_clone/users/admin.py
@@ -7,11 +7,33 @@ from instagram_clone.users.forms import UserChangeForm, UserCreationForm
 User = get_user_model()
 
 
+class MyUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = User
+
+
+class MyUserCreationForm(UserCreationForm):
+    error_message = UserCreationForm.error_messages.update({
+        'duplicated_username': 'This username has already been taken.'
+    })
+
+    class Meta(UserCreationForm.Meta):
+        model = User
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        try:
+            User.objects.get(username=username)
+        except User.DoesNotExist:
+            return username
+        raise forms.ValidationError(self.error_messages['duplicated_username'])
+
+
 @admin.register(User)
 class UserAdmin(auth_admin.UserAdmin):
 
     form = UserChangeForm
     add_form = UserCreationForm
-    fieldsets = (("User", {"fields": ("name",)}),) + auth_admin.UserAdmin.fieldsets
+    fieldsets = (("User", {"fields": ("name", "followers", "following")}),) + auth_admin.UserAdmin.fieldsets
     list_display = ["username", "name", "is_superuser"]
     search_fields = ["name"]
