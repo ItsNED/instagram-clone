4162d69dcd94bb02977a98737c868a427c4ce112
Inmobi Korea <taki1984@naver.com>
2018-10-29 17:45:18 +0900

    # creating notifications(Follow, Comment, Like)

--- instagram_clone/users/views.py
+++ instagram_clone/users/views.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from . import models, serializers
+from instagram_clone.notifications import views as notification_views
 
 
 class ExploreUsers(APIView):
@@ -21,8 +22,6 @@ class FollowUser(APIView):
 
         user = request.user
 
-        # create notification for follow
-
         try:
             user_to_follow = models.User.objects.get(id=user_id)
         except models.User.DoesNotExist:
@@ -30,6 +29,8 @@ class FollowUser(APIView):
 
         user.following.add(user_to_follow)
         user.save()
+
+        notification_views.create_notification(user, user_to_follow, 'follow')
 
         return Response(status=status.HTTP_200_OK)
 
