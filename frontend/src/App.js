6651d478aa76c2953f6d52950145b53f585a5c8b
Inmobi Korea <taki1984@naver.com>
2018-11-06 18:14:12 +0900

    # Setting up the Redux Store with Multiple Reducers

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,10 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import style from './App.module.scss';
+import styles from './App.module.scss';
 
 class App extends Component {
   render() {
     return (
-      <div className={style.App}>
-        <header className={style.appHeader}>
-          <img src={logo} className={style.appLogo} alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className={style.appLink}
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <div className={styles.App} />
     );
   }
 }
