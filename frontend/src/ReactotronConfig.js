f59d324d500001bb538860a59b6dadd337e4dcf9
Inmobi Korea <taki1984@naver.com>
2018-11-08 14:03:47 +0900

    # install Redux devtools with yarn add redux-devtools-extension --dev

--- frontend/src/ReactotronConfig.js
+++ frontend/src/ReactotronConfig.js
@@ -3,4 +3,4 @@ import { reactotronRedux } from "reactotron-redux";
 
 Reactotron.configure({ name : "insta-clone" }).use(reactotronRedux()).connect();
 
-export default Reactotron;
+export default Reactotron; 
