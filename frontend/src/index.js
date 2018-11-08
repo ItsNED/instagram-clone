04564f2e5fa781579a052cc810a61182758cbb14
Inmobi Korea <taki1984@naver.com>
2018-11-08 14:04:56 +0900

    # disable reactotron

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,4 +1,4 @@
-import "ReactotronConfig";
+// import "ReactotronConfig";
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
--- frontend/src/redux/configureStore.js
+++ frontend/src/redux/configureStore.js
@@ -4,7 +4,7 @@ import { routerReducer, routerMiddleware } from "react-router-redux";
 import createHistory from "history/createBrowserHistory";
 import { composeWithDevTools } from "redux-devtools-extension";
 import users from "redux/modules/users";
-import Reactotron from "ReactotronConfig";
+// import Reactotron from "ReactotronConfig";
 
 const env = process.env.NODE_ENV;
 
@@ -25,7 +25,7 @@ const reducer = combineReducers({
 let store;
 
 if (env === "development") {
-    store = initialState => Reactotron.createStore(
+    store = initialState => createStore(
         reducer, 
         composeWithDevTools(applyMiddleware(...middlewares))
         );
