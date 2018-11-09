0dfe649f003abd6e6666f9a4623e096ba5cfc640
Inmobi Korea <taki1984@naver.com>
2018-11-09 10:58:38 +0900

    # styling the Footer

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-expressions */
 import React from "react";
-import styles from "./styles.scss";
+import styles from "./styles.module.scss";
 
 const Footer = (props, context) => (
     <footer className={styles.footer}>
